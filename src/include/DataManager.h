a8c528cc315ae5b0ab482f4dc14a57523938f37b
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2025-06-21 14:01:45 -0300

    Refactor DataManager to improve sequence loading and enhance type clarity

--- src/include/DataManager.h
+++ src/include/DataManager.h
@@ -4,18 +4,24 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <utility>
 
-using PropertiesMap = std::map<std::string, std::vector<double>>;
+using PropertiesMap = std::map<std::string, std::vector<double> >;
+using SequenceData = std::pair<std::string, std::string>; // <ID, Sequência>
 
 class DataManager {
 public:
     DataManager(std::string data_path, std::string sequence_path);
+
     bool setup_data_files();
+
     PropertiesMap load_and_augment_properties();
-    std::vector<std::string> load_sequences();
+
+    std::vector<SequenceData> load_sequences();
 
 private:
-    bool _download_file(const std::string& url, const std::string& filename);
+    bool _download_file(const std::string &url, const std::string &filename);
+
     std::string data_filepath;
     std::string sequence_filepath;
 };
