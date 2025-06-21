95ad12d2e1ff7be7feb9c2915aad6543bd98b2b0
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2025-06-21 14:03:57 -0300

    Refactor BenchmarkRunner to use SequenceData and update processor types

--- src/include/BenchmarkRunner.h
+++ src/include/BenchmarkRunner.h
@@ -1,21 +1,32 @@
 #ifndef BENCHMARKRUNNER_H
 #define BENCHMARKRUNNER_H
+
 #include "PseKNCProcessor.h"
-#include "ReportGenerator.h"
 #include <memory>
+#include <vector>
+#ifdef WITH_CUDA
+#include "include/GPUProcessor.h"
+#endif
+#include "include/OMPProcessor.h"
+
 
 class BenchmarkRunner {
 public:
-    BenchmarkRunner(std::vector<std::string> sequences, PseKNCParams params, const PropertiesMap& properties);
-    void run(const std::vector<long long>& num_sequences_to_run);
+    BenchmarkRunner(std::vector<SequenceData> sequences,
+                    PseKNCParams params,
+                    const PropertiesMap &properties);
+
+    void run(const std::vector<long long> &num_sequences_to_run);
+
 private:
-    std::vector<std::string> main_sequences;
+    std::vector<SequenceData> main_sequences;
     PseKNCParams params;
-    const PropertiesMap& properties;
+    const PropertiesMap &properties;
     std::unique_ptr<PseKNCProcessor> cpu_processor;
-    std::unique_ptr<PseKNCProcessor> omp_processor;
+    std::unique_ptr<OMPProcessor> omp_processor;
 #ifdef WITH_CUDA
-    std::unique_ptr<PseKNCProcessor> gpu_processor;
+    std::unique_ptr<GPUProcessor> gpu_processor;
 #endif
 };
+
 #endif // BENCHMARKRUNNER_H
