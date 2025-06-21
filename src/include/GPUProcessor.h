fd8cb0accb539592eecfd4dbfdef6c3af96c86b1
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2025-06-21 14:00:53 -0300

    Refactor GPUProcessor to support batch processing of sequences

--- src/include/GPUProcessor.h
+++ src/include/GPUProcessor.h
@@ -9,19 +9,15 @@ public:
 
     ~GPUProcessor();
 
-    std::vector<double> process(const std::string &sequence) override;
+    std::vector<std::vector<double> > process_batch(const std::vector<std::string> &sequences);
 
 private:
-    std::vector<int> prepare_ktuples_to_indices(const std::string &sequence);
+    std::vector<double> process(const std::string &sequence) override;
 
     void setup_gpu_data();
 
-    double calculate_theta(const int *d_k_tuple_indices, size_t num_k_tuples, int lambda_i);
-
     double *d_property_table = nullptr;
-    int *d_k_tuple_indices = nullptr;
     std::map<std::string, int> k_tuple_to_index;
     size_t num_properties;
-    size_t max_k_tuples_allocated = 0;
 };
 #endif // GPUPROCESSOR_H
