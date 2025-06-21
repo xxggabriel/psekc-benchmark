648eed670b5612fd8e5cf6784d421e31b04068ab
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2025-06-21 09:44:14 -0300

    Refactor BenchmarkRunner to support multiple sequences in the constructor and update run method signature

--- src/include/BenchmarkRunner.h
+++ src/include/BenchmarkRunner.h
@@ -1,25 +1,21 @@
 #ifndef BENCHMARKRUNNER_H
 #define BENCHMARKRUNNER_H
-
 #include "PseKNCProcessor.h"
-#include "ReportGenerator.h" // Inclui a definição de BenchmarkResult
-#include <memory> // Para std::unique_ptr
+#include "ReportGenerator.h"
+#include <memory>
 
 class BenchmarkRunner {
 public:
-    BenchmarkRunner(std::string sequence, PseKNCParams params, const PropertiesMap& properties);
-    void run(const std::vector<long long>& sizes);
-
+    BenchmarkRunner(std::vector<std::string> sequences, PseKNCParams params, const PropertiesMap& properties);
+    void run(const std::vector<long long>& num_sequences_to_run);
 private:
-    std::string main_sequence;
+    std::vector<std::string> main_sequences;
     PseKNCParams params;
     const PropertiesMap& properties;
-
     std::unique_ptr<PseKNCProcessor> cpu_processor;
     std::unique_ptr<PseKNCProcessor> omp_processor;
 #ifdef WITH_CUDA
     std::unique_ptr<PseKNCProcessor> gpu_processor;
 #endif
 };
-
 #endif // BENCHMARKRUNNER_H
